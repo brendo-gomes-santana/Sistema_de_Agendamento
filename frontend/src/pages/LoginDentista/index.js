b463e3d8072e4008a50ca2468b5e3299aef22800
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-05-24 18:09:59 -0300

    criando a estrutura de rotas

---
+++ frontend/src/pages/LoginDentista/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import api from '../../service'
+import { BiUser, BiLoaderCircle } from 'react-icons/bi'
+
+import './style.scss'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { toast } from 'react-toastify'
+
+export default function LoginDentista() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [loading, setLoading ] = useState(false)
+
+    async function handleLoginUser(e){
+        setLoading(true)
+        e.preventDefault()
+
+        if(!email || !password){
+            toast.error('Preenchar Todas as informações')
+            setLoading(false)
+            return;
+        }
+
+        await api.post('/session/user',{
+            email,
+            password
+        }).then((r)=>{
+            console.log(r.data)
+            setLoading(false)
+        }).catch((err)=> {
+            toast.error(err.response.data.error)
+            setLoading(false)
+        })
+        
+    }
+    return (
+        <main className='containerLoginUser'>
+            <h1><BiUser/> Login</h1>
+            <form className='formUserLogin' onSubmit={handleLoginUser}>
+                <label>
+                    Email:  <Input type='text' placeholder='Digite seu email' 
+                                value={email} onChange={ v => setEmail(v.target.value)}/>
+                </label>
+                <label>
+                    Senha: <Input type='password' placeholder='Digite sua senha' 
+                                value={password} onChange={ v => setPassword(v.target.value)}/>
+                </label>
+                <Button type='submit'>{loading? <BiLoaderCircle className='loader'/> : <span>Entrar</span>}</Button>
+            </form>
+        </main>
+    )
+    }
+
